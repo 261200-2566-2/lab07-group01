469c28f1adbe13ff4bcb192e2c1d088a6829f4ac
GOLF2546 <Pattarapolnanakorn@gmail.com>
2023-12-07 08:52:00 +0700

    Commit version1

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,36 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.sql.SQLOutput;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
+        //Implement initial hashmap
+        HashMap<String> map = new HashMap<>();
+        map.add("Apple");
+        map.add("Pineapple");
+        map.add("Orange");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        System.out.println("Size : "+map.size());
+        System.out.println("Contains `Banana` : "+map.contains("Banana"));
+        System.out.println("Contains `Apple` : "+map.contains("Apple"));
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        map.remove("Apple");
+
+        System.out.println("Map size after removing `Apple` : "+map.size());
+
+        map.printElement();
+        map.clear();
+        map.printElement();
+
+        HashMap <Integer> map2 = new HashMap<>();
+        map2.add(1);
+        map2.add(2);
+        map2.add(3);
+
+        System.out.println("Size before Clear: "+map2.size());
+        System.out.println("Map element : ");
+        map2.printElement();
+        map2.clear();
+        System.out.println("Size after Clear: "+map2.size());
+        map2.printElement();
     }
 }
---
+++ src/Set.java
@@ -0,0 +1,5 @@
+import java.util.Collection;
+import java.util.HashMap;
+public interface Set<E> extends Collection<E>{
+    
+}
